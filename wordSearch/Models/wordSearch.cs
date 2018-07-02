68e032eae3a8e694f6bb2324af75a92ab29e456c
bunnymarie <bunnymarie@protonmail.com>
2018-07-01 21:59:54 -0700

    Adds ininitial README file.

--- wordSearch/Models/wordSearch.cs
+++ wordSearch/Models/wordSearch.cs
@@ -2,7 +2,7 @@ namespace wordSearch
 {
   public class RepeatCounter
   {
-    public static bool returnTest(string userInput)
+    public static bool returnTest1(string userInput)
       {
           string[] userSplit = userInput.Split();
           int foundNumbers = 0;
@@ -22,5 +22,66 @@ namespace wordSearch
               return false;
             }
       }
+      public static bool returnTest2(string userInput)
+        {
+            string[] userSplit = userInput.Split();
+            int foundNumbers = 0;
+
+            for(int i = 0; i < userSplit.Length; i++)
+              {
+                if (userSplit[i].Contains("you"))
+                {
+                  foundNumbers += 1;
+                }
+              }
+
+              if(foundNumbers == 4){
+                return true;
+              }
+              else{
+                return false;
+              }
+        }
+        public static bool returnTest3(string userInput)
+          {
+              string[] userSplit = userInput.Split();
+              int foundNumbers = 0;
+
+              for(int i = 0; i < userSplit.Length; i++)
+                {
+                  if (userSplit[i].Contains("aenean"))
+                  {
+                    foundNumbers += 1;
+                  }
+                }
+
+                if(foundNumbers == 3){
+                  return true;
+                }
+                else{
+                  return false;
+                }
+          }
+          public static bool returnTest4(string userInput)
+            {
+                string userLower = userInput.ToLower();
+                string[] userSplit = userLower.Split();
+                int foundNumbers = 0;
+
+                for(int i = 0; i < userSplit.Length; i++)
+                  {
+                    if (userSplit[i].Contains("lorem"))
+                    {
+                      foundNumbers += 1;
+                    }
+                  }
+
+                  if(foundNumbers == 1){
+                    return true;
+                  }
+                  else{
+                    return false;
+                  }
+            }
   }
 }
