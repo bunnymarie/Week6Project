0314c5afb869c7b1f25f2bfcf2247e41d96bfa94
bunnymarie <bunnymarie@protonmail.com>
2018-07-01 21:32:03 -0700

    Adds second test that passes as true.

--- wordSearch.Tests/ModelsTests/wordSearch.Tests.cs
+++ wordSearch.Tests/ModelsTests/wordSearch.Tests.cs
@@ -9,7 +9,12 @@ namespace wordSearch.Tests
     [TestMethod]
     public void IsRepeatCounter_FoundFourYous_True()
     {
-      Assert.AreEqual(true, RepeatCounter.returnTest("How are you? I hope to see you soon. I love you and miss you."));
+      Assert.AreEqual(true, RepeatCounter.returnTest1("How are you? I hope to see you soon. I love you and miss you."));
+    }
+    [TestMethod]
+    public void IsRepeatCounter_FoundFourYous_False()
+    {
+      Assert.AreEqual(false, RepeatCounter.returnTest2("I hope they have a great day."));
     }
   }
 }
